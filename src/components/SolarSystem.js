75f05f9c3642e1052c92001818b9b0bda39a369d
Rauã <rauavasconcelos22@gmail.com>
2023-05-04 18:40:52 -0300

    requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,13 +1,30 @@
+import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
-function SolarSystem() {
-  return (
-    <div>
-      <div data-testid="solar-system" />
-      <Title />
-    </div>
+class SolarSystem extends React.Component {
+  render() {
+    const PlanetsItems = planets.map((planet) => {
+      console.log(planet);
+      return (
+        <li key={ planet.name }>
+          <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+        </li>
+      );
+    });
+    return (
+      <div>
 
-  );
+        <div data-testid="solar-system" />
+        <Title headline="Planetas" />
+        <ul>
+          {PlanetsItems}
+        </ul>
+      </div>
+
+    );
+  }
 }
 
 export default SolarSystem;
