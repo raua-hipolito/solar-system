17f8681aa52d18ffa2434f74817148ca3d5c64dc
Rauã <rauavasconcelos22@gmail.com>
2024-06-06 14:50:23 -0300

    apagando arquivos para iniciar novamente o projeto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Header from './components/Header';
-import SolarSystem from './components/SolarSystem';
-import Missions from './components/Missions';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
-        <SolarSystem />
-        <Missions />
+        <h1>React App</h1>
       </div>
     );
   }
