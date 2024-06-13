f754687811326924524556ee6b363d4ebea7efcd
Rauã <rauavasconcelos22@gmail.com>
2024-06-13 18:27:24 -0300

    ✅ creating SolarSystem component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>React App</h1>
+      <>
         <Header />
-      </div>
+        <SolarSystem />
+      </>
     );
   }
 }
